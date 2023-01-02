2d6d422f558c7b8c3f1bf28eacc53778d0c3f8df
Roseak-lin <roseak.lin@gmail.com>
2023-01-01 19:52:06 -0500

    made chat page functional, but does not make any data transfer with server

--- frontend/projectw-frontend/src/components/ChatTest.vue
+++ frontend/projectw-frontend/src/components/ChatTest.vue
@@ -1,18 +1,8 @@
 <template>
-    <textarea id="chat-log" cols="100" rows="20"></textarea><br />
-    <input
-        id="chat-message-input"
-        onkeyup="chatMsgKeyUp(e)"
-        type="text"
-        size="100"
-    />
+    <textarea ref="chatlog" id="chat-log" cols="100" rows="20"></textarea><br />
+    <input id="chat-message-input" @keyup="chatMsgKeyUp($event)" type="text" size="100" />
     <br />
-    <input
-        id="chat-message-submit"
-        onclick="chatMsgSubmit"
-        type="button"
-        value="Send"
-    />
+    <input id="chat-message-submit" @click="chatMsgSubmit($event)" type="button" value="Send" />
     <div ref="room_name" id="room_name" v-html="roomNameJson"></div>
     <!-- need to change from div. div is temp placeholder -->
 </template>
@@ -21,22 +11,22 @@
 export default {
     mounted() {
         document.querySelector("#chat-message-input").focus();
-        const roomName = JSON.parse(
-            document.querySelector("#room_name").textContent
-        );
+        //     const roomName = JSON.parse(
+        //         document.querySelector("#room_name").textContent
+        //     );
 
-        const chatSocket = new WebSocket(
-            "ws://" + window.location.host + "/ws/chat/" + roomName + "/"
-        );
+        //     const chatSocket = new WebSocket(
+        //         new WebSocket('ws://' + window.location.host + '/ws/match/' + roomName + '/')
+        //     );
 
-        chatSocket.onmessage = function (e) {
-            const data = JSON.parse(e.data);
-            document.querySelector("#chat-log").value += data.message + "\n";
-        };
+        //     chatSocket.onmessage = function (e) {
+        //         const data = JSON.parse(e.data);
+        //         document.querySelector("#chat-log").value += data.message + "\n";
+        //     };
 
-        chatSocket.onclose = function (e) {
-            console.error("Chat socket closed unexpectedly");
-        };
+        //     chatSocket.onclose = function (e) {
+        //         console.error("Chat socket closed unexpectedly");
+        //     };
     },
     methods: {
         chatMsgKeyUp(e) {
@@ -50,11 +40,12 @@ export default {
                 "#chat-message-input"
             );
             const message = messageInputDom.value;
-            chatSocket.send(
-                JSON.stringify({
-                    message: message,
-                })
-            );
+            // chatSocket.send(
+            //     JSON.stringify({
+            //         message: message,
+            //     })
+            // );
+            document.querySelector("#chat-log").value += message + "\n";
             messageInputDom.value = "";
         },
     },
