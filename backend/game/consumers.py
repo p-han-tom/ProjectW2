af7ca586eae953a725ebbd64bf626e2e2e3f11b5
p-han-tom <hantom0127@gmail.com>
2023-01-06 02:11:43 -0500

    start_game impl

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -20,10 +20,11 @@ class PlayerConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_name, self.channel_name)
         redis_client = redis.Redis(connection_pool=pool)
         lobby_state = json.loads(redis_client.get(self.room_name))
+        
 
         if (lobby_state != None):
-            lobby_state.update({"players": lobby_state.players.append(self.channel_name)})
-            redis_client.set(self.room_name, lobby_state)
+            lobby_state.get("players").append(self.channel_name)
+            redis_client.set(self.room_name, json.dumps(lobby_state))
             await self.accept()
         else:
             self.close()
@@ -32,16 +33,22 @@ class PlayerConsumer(AsyncWebsocketConsumer):
         # Leave room group
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
+        # Delete from redis
+        redis_client = redis.Redis(connection_pool=pool)
+        lobby_state = json.loads(redis_client.get(self.room_name))
+        lobby_state.get("players").remove(self.channel_name)
+        redis_client.set(self.room_name, json.dumps(lobby_state))
+
     # Receive message from WebSocket
     async def receive(self, text_data):
         payload = json.loads(text_data)
-        eventType = payload.eventType
+        eventType = payload.get("eventType")
 
         # Send message to room group
         match eventType:
             case "start_game":
                 redis_client = redis.Redis(connection_pool=pool)
-                lobby_state = redis_client.get(self.room_name)
+                lobby_state = json.loads(redis_client.get(self.room_name))
                 # initialize game state at payload.key
 
                 # -> initialize board
@@ -54,32 +61,37 @@ class PlayerConsumer(AsyncWebsocketConsumer):
                         }
 
                 # -> intialize units
-                units = [[None] * 8]
+                units = [None] * 8
                 for i in range(4):
                     units[i] = {
                         "unit_id": "duelist",
-                        "player_id": lobby_state.players[0],
+                        "player_id": lobby_state["players"][0],
                         "hp": 4
                     }
-                for i in range(4,8):
+                for i in range(4, 8):
                     units[i] = {
                         "unit_id": "duelist",
-                        "player_id": lobby_state.players[1],
+                        "player_id": lobby_state["players"][1],
                         "hp": 4
                     }
+
+                # upsert to lobby_state
                 lobby_state.update({"board": board, "units": units})
                 redis_client.set(self.room_name, json.dumps(lobby_state))
+                print(json.loads(redis_client.get(self.room_name)))
 
             case "move":
                 try:
                     entity_table[payload.unit_type].move(payload)
                     await self.channel_layer.group_send(
-                        self.room_name, {"type": "move", "message": "Unit moved on board"}
+                        self.room_name, {"type": "move",
+                                         "message": "Unit moved on board"}
                     )
                 except:
                     await self.channel_layer.group_send(
                         self.room_name, {
-                            "type": "action_failed", "message": "Action was invalid, states may not be synced"}
+                            "type": "action_failed", "message": "Action was invalid, states may not be synced"
+                        }
                     )
 
     # Receive message from room group
